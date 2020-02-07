5c83247110768414c0c88acb7567e5dbbd4cade6
bguiz <bguiz@users.noreply.github.com>
2020-02-07 11:01:38 +0800

    feat: remove bislr

--- src/html.js
+++ src/html.js
@@ -13,12 +13,6 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           {this.props.headComponents}
-          <script
-            key="autopilot"
-            dangerouslySetInnerHTML={{
-              __html: `(function(o){var b="https://stagingapi.bislr.net:8100/anywhere/",t="8a0a332b06164f32b17a159bb019197179692aebcadc44859251b247cee563f3",a=window.AutopilotAnywhere={_runQueue:[],run:function(){this._runQueue.push(arguments);}},c=encodeURIComponent,s="SCRIPT",d=document,l=d.getElementsByTagName(s)[0],p="t="+c(d.title||"")+"&u="+c(d.location.href||"")+"&r="+c(d.referrer||""),j="text/javascript",z,y;if(!window.Autopilot) window.Autopilot=a;if(o.app) p="devmode=true&"+p;z=function(src,asy){var e=d.createElement(s);e.src=src;e.type=j;e.async=asy;l.parentNode.insertBefore(e,l);};y=function(){z(b+t+'?'+p,true);};if(window.attachEvent){window.attachEvent("onload",y);}else{window.addEventListener("load",y,false);}})({});`
-            }}
-          ></script>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
